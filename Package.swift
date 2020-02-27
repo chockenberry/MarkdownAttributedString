b2e1ea7b2683673e2618b7f7ab0b68342e13583d
Pierluigi Cifani <pcifani@theleftbit.com>
2020-02-27 18:43:32 +0100

    Add Swift.package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MarkdownAttributedString",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "MarkdownAttributedString",
+            targets: ["MarkdownAttributedString"]),
+    ],
+    targets: [
+        .target(
+            name: "MarkdownAttributedString",
+            dependencies: []),
+        .testTarget(
+            name: "MarkdownAttributedStringTests",
+            dependencies: ["MarkdownAttributedString"]),
+    ]
+)
